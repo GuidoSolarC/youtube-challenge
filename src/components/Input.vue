0bc2770cc45d95b28e6e0fe73f40ca1bc75537c9
Guido Solar <guido.solar.c@gmail.com>
2022-09-07 21:28:34 -0300

    Se controla si video existe en BD

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -6,7 +6,7 @@
             <label class="fs-4 fw-bold d-flex align-self-start" for="input_anadir">Añadir nuevo video</label>
             <div class="inputUrl input-group mb-3">
               <input type="text" v-model="urlAgregar" id="input_anadir" class="form-control" placeholder="Añadir" aria-label="Añadir" aria-describedby="button-addon2">
-              <button class="inputUrl__button btn btn-primary" type="button" id="button-addon2" v-on:click="comprobarURL()">Añadir</button>
+              <button class="inputUrl__button btn btn-primary" type="button" id="button-addon2" v-on:click="comprobarUrl()">Añadir</button>
             </div>
           </div>
       </div>
@@ -22,10 +22,26 @@ export default {
         obtenerIdVideo: function (url) {
             return url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/)
         },    
-        // Método para validar si URL de youtube es válida
-        comprobarURL: function () {
+        // Funcion para comprobar que video existe o no en nuestra BD
+        comprobarUrl: function () {
             const idVideo = this.obtenerIdVideo(this.urlAgregar)
-            this.axios.get(this.apiGoogle + idVideo + this.keyGoogle + '&part=snippet', { 
+            this.axios.get(this.apiAWS + 'videos/' + idVideo[1] , { 
+			}).then(response => {                
+                // Si no obtenemos registros, entonces insertamos nuevo video
+                if(Object.keys(response.data).length === 0){
+                    this.urlValida(idVideo)
+                } else {                    
+                    this.$alertify.error('El video ya está registrado, intenta con otro :)');
+                }
+			}).catch(error => {
+                this.$alertify.error('Ha ocurrido un error comprobando la url');
+				console.log(error)
+			})       
+        }, 
+        // Método para validar si URL de youtube es válida
+        urlValida: function () {
+            const idVideo = this.obtenerIdVideo(this.urlAgregar)
+            this.axios.get(this.apiGoogle + idVideo[1] + this.keyGoogle + '&part=snippet', { 
 			}).then(response => {
                 if(response.data.items.length === 1){
                     this.agregarURL(idVideo)
